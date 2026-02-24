b34af1d6f186eaa9cf080796a8c2837501cb0a71
info@od-connect.com <info@od-connect.com>
2026-02-24 11:13:54 +0700

    fix paths for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: 'https://kliawpan.github.io/popreceive/', 
+  base: 'https://odconnect.github.io/receive-tracking-order/', 
 })
